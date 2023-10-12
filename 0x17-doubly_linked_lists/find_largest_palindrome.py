afd26843af436e8429aa14b51ca4ea00b9ba1b31
AYOUBZAKI1 <ayoubbenzaki2209@gmail.com>
2023-10-13 00:19:34 +0100

    C prog

--- 0x17-doubly_linked_lists/find_largest_palindrome.py
+++ 0x17-doubly_linked_lists/find_largest_palindrome.py
@@ -1,14 +1,10 @@
-# Function to check if a number is a palindrome
-def is_palindrome(num):
-    return str(num) == str(num)[::-1]
-
 largest_palindrome = 0
 
-# Iterate through all possible products of two 3-digit numbers
+# Iterate through all possible 3-digit numbers
 for num1 in range(100, 1000):
     for num2 in range(100, 1000):
         product = num1 * num2
-        if is_palindrome(product) and product > largest_palindrome:
+        if str(product) == str(product)[::-1] and product > largest_palindrome:
             largest_palindrome = product
 
 # Save the result to the file 102-result
